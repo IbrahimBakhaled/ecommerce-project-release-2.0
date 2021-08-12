2288f1d17e50babf246d05042f72ae9a277fd564
IbrahimBakhaled <bakhaledibrahim@gmail.com>
2021-08-12 22:21:05 +0100

    finishing pagination both homePage and searched Products pagination

--- Frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ Frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -13,51 +13,69 @@ import {ActivatedRoute} from "@angular/router";
 export class ProductListComponent implements OnInit {
 
 
-  products! : Product[];
-  currentCategoryId! : number;
-  searchMode! : boolean;
+  products: Product[] = [];
+  currentCategoryId: number = 1;
+  searchMode: boolean = false;
+  previousCategoryId: number = 1;
+
+  // proprreties pour la pagination
+  thePageNumber: number = 1;
+  thePageSize: number = 15;
+  theTotalElements: number = 0;
 
 
-  constructor(private productService : ProductService,
-              private route: ActivatedRoute) { }
+  previousKeyword!: string;
+
+
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(()=> {
+    this.route.paramMap.subscribe(() => {
       this.listProducts();
     });
 
   }
 
 
-  listProducts(){
+  listProducts() {
     this.searchMode = this.route.snapshot.paramMap.has('keyword');
-    if (this.searchMode){
+    if (this.searchMode) {
       this.handleSearchProducts();
-    }else {
+    } else {
       this.handleListProducts();
     }
 
 
   }
 
-  handleSearchProducts(){
-    const theKeyword : string = this.route.snapshot.paramMap.get('keyword')!;
+  handleSearchProducts() {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    // si en a different keyword de precedent en va mettre thepageNumber a 1
+
+    if (this.previousKeyword != theKeyword){
+      this.thePageNumber=1;
+    }
+    this.previousKeyword = theKeyword;
+    console.log(`keyword=${theKeyword}, thePAgeNumber=${this.thePageNumber}`);
+
+
+
+
+
 
     // chercher le produit à travers clé "keyword"
-
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    );
+    this.productService.searchProductsPaginate(this.thePageNumber -1, this.thePageSize, theKeyword).subscribe(this.processResult())
   }
 
-  handleListProducts(){
+  handleListProducts() {
     // verifier si "id" est valable
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
 
 
-    if (hasCategoryId){
+    if (hasCategoryId) {
       // prendre l'id comme param string est le converté "string" a "number" avec "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
     } else {
@@ -66,12 +84,38 @@ export class ProductListComponent implements OnInit {
     }
 
 
-    // get products
+    // verifier si en a un different categorie
+    // note: angular sera utilisé le component si il est actuelllement verifier
+    // si en a differnetn categorie id de la precent alors definer le a 1 (thePageNumber = 1)
+    if (this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+    this.previousCategoryId = this.currentCategoryId;
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`);
+
+
+    // get products
+    this.productService.getProductListPaginate(this.thePageNumber - 1,
+      this.thePageSize,
+      this.currentCategoryId)
+      .subscribe(this.processResult());
+
+  }
+
+   public processResult() {
+    return data => {
+      this.products = data._embedded.products;
+        this.thePageNumber = data.page.number + 1;
+        this.thePageSize = data.page.size;
+        this.theTotalElements = data.page.totalElements;
+
+    };
+  }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber=1;
+    this.listProducts();
   }
 }
--- Frontend/angular-ecommerce/src/app/services/product.service.ts
+++ Frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -17,6 +17,12 @@ export class ProductService {
 
   constructor(private httpClient:HttpClient) { }
 
+
+
+
+
+
+
   public getProductList(theCategoryId : number): Observable<Product[]>{
 
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
@@ -27,6 +33,30 @@ export class ProductService {
     );
   }
 
+
+
+
+
+
+  public getProductListPaginate(thePage: number,
+                                thePageSize: number,
+                                theCategoryId : number): Observable<GetResponseProducts>{
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
+
+
+
+
+
+
+
+
   public getProductCategories() : Observable<ProductCategory[]>{
 
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
@@ -34,6 +64,13 @@ export class ProductService {
     );
 
   }
+
+
+
+
+
+
+
 
   public searchProducts(theKeyword: string) :Observable<Product[]>{
 
@@ -45,16 +82,56 @@ export class ProductService {
     );
   }
 
+
+
+
+
+
+
+
+  public searchProductsPaginate(thePage: number,
+                                thePageSize: number,
+                                theKeyword : string): Observable<GetResponseProducts>{
+
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyword}`
+                    + `&page=${thePage}&size=${thePageSize}`;
+
+
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
+
+
+
+
+
+
+
+
   getProduct(theProductId: number):Observable<Product> {
     const productUrl = `${this.baseUrl}/${theProductId}`;
     return this.httpClient.get<Product>(productUrl);
   }
+
+
+
+
+
+
 }
 
 
 interface GetResponseProducts {
   _embedded :{
     products: Product[];
+  },
+  // we added these lines (page: size,totalelements,totalpages,number) because before adding them spring data rest by default
+  // gives us a responses with thees parameters in last lines http://localhost:8080/api/products?page=0&size=500
+  page:{
+    size: number,
+    totalElements : number,
+    totalPages: number,
+    number: number
   }
 
 }
