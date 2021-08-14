a2790a2cb9174926546d06c4f2cdb65ef9e22fe8
IbrahimBakhaled <bakhaledibrahim@gmail.com>
2021-08-14 18:52:14 +0100

    completing drop-down list of countries and states

--- Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ Frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit, ɵConsole} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {ShoppylandFormService} from "../../services/shoppyland-form.service";
-import {JSONSchema4Object} from "json-schema";
+import {Country} from "../../common/country";
+import {State} from "../../common/state";
 
 @Component({
   selector: 'app-checkout',
@@ -19,6 +20,12 @@ export class CheckoutComponent implements OnInit {
 
   creditCardYears : number[] = [];
   creditCardMonths : number[] = [];
+
+
+
+  countries: Country[] = [];
+  shippingAddressStates : State[] = [];
+  billingAddressStates : State[] = [];
 
   constructor(public formBuilder: FormBuilder,
               private shoppylandFormService : ShoppylandFormService) { }
@@ -68,6 +75,14 @@ export class CheckoutComponent implements OnInit {
      );
 
 
+     this.shoppylandFormService.getCountries().subscribe(
+       data => {
+         console.log("Retrieved countries: " + JSON.stringify(data));
+         this.countries = data;
+       }
+     );
+
+
      this.shoppylandFormService.getCreditCardYears().subscribe(
        data =>{
          console.log("Retrieved credit card years: " + JSON.stringify(data));
@@ -110,5 +125,28 @@ export class CheckoutComponent implements OnInit {
         this.creditCardMonths = data;
       }
     )
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup!.value.country.code;
+    const countryName = formGroup!.value.country.name;
+
+    console.log(`${formGroupName} country code : ${countryCode}`);
+    console.log(`${formGroupName} country name : ${countryName}`);
+
+    this.shoppylandFormService.getStates(countryCode).subscribe(
+      data => {
+        if(formGroupName === 'shippingAddress'){
+          this.shippingAddressStates = data;
+        } else {
+          this.billingAddressStates = data;
+        }
+
+        // selecte le premier state par defaut
+        formGroup!.get('state')!.setValue(data[0]);
+      }
+    );
   }
 }
--- Frontend/angular-ecommerce/src/app/services/shoppyland-form.service.ts
+++ Frontend/angular-ecommerce/src/app/services/shoppyland-form.service.ts
@@ -1,15 +1,41 @@
 import { Injectable } from '@angular/core';
 import {Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Country} from "../common/country";
+import {map} from "rxjs/operators";
+import {State} from "../common/state";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppylandFormService {
 
-  constructor() { }
+
+  // pays & regions urls
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private stateUrl = 'http://localhost:8080/api/states';
 
 
 
+  // injecte httpClient dans le constructeur
+  constructor( private httpClient : HttpClient) { }
+
+
+getCountries() : Observable<Country[]> {
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+}
+
+getStates(theCountryCode : string): Observable<State[]>{
+
+    //chercher url
+  const searchStatesurl = `${this.stateUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+  return this.httpClient.get<GetResponseStates>(searchStatesurl).pipe(
+    map(response => response._embedded.states)
+  );
+}
 
 
   getCreditCardMonths (startMonth : number) : Observable<number[]>{
@@ -47,5 +73,19 @@ export class ShoppylandFormService {
 
     return of (data);
 
+  }
+}
+
+//Unwraps the JSON from Spring Data Rest
+interface  GetResponseCountries {
+  _embedded : {
+    countries: Country[];
+  }
+
+}
+
+interface GetResponseStates{
+  _embedded: {
+    states : State[];
   }
 }
